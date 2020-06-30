1b3351ca52e01c23a69b0a13da96f766e4c812e1
Yk Snow <hyuki3110@gmail.com>
2020-07-01 04:17:00 +0900

    add CV page

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,8 +5,14 @@ import Content from "../components/content"
 export default function About() {
   return (
     <div>
-      <Layout title="About"/>
-      <Content> </Content>
+      <Layout title="About" />
+      <Content>
+        <iframe
+          src="https://drive.google.com/file/d/1etUS-Jnu6yro6zCG8i4XvLv4wXq4zOV2/preview"
+          width="800"
+          height="800"
+        ></iframe>
+      </Content>
     </div>
   )
 }
